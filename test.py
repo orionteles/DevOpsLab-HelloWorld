1a76823575b6a60ed335239e34bc1c52900bc789
Orion Teles de Mesquita <orion.mesquita@poupex.com.br>
2018-09-05 18:14:11 -0300

    [ UPDATE ] Testando CI

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Orion Teles de Mesquita.")
+        self.assertRegex(result.data.decode(), "Orion")
 
 
 if __name__ == "__main__":
