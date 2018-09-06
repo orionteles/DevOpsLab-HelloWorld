6c46b37fbd59e8f5f2862b6fd51cefeb26d35459
Orion Teles de Mesquita <orion.mesquita@poupex.com.br>
2018-09-06 15:23:43 -0300

    [ UPDATE ] Alterando título correto

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "PoupexOrion")
+        self.assertRegex(result.data.decode(), "Poupex")
 
 
 if __name__ == "__main__":
